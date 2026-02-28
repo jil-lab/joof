4c82b2f6a4ad80b45e4683fe703b6c436f10630a
Jileyitayo <jileyitayo@gmail.com>
2026-02-28 00:44:48 +0000

    added email templates

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('EMAIL_SMTP_HOST', 'smtp.resend.com'),
+        port: env.int('EMAIL_SMTP_PORT', 465),
+        auth: {
+          user: env('EMAIL_SMTP_USER', 'resend'),
+          pass: env('EMAIL_SMTP_PASSWORD'),
+        },
+        secure: true, // Use SSL
+      },
+      settings: {
+        defaultFrom: env('EMAIL_DEFAULT_FROM'),
+        defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO'),
+      },
+    },
+  },
+});
