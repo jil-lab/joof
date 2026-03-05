8bec2d220d6ff6adf8fe0d89cbb8810235fc5bb6
Jileyitayo <jileyitayo@gmail.com>
2026-03-05 00:29:40 +0000

    Add CORS configuration to middlewares and update .env.example with allowed origins

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,19 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: env.array('CORS_ORIGINS', [
+        'http://localhost:5173', // Vite dev server
+        'http://localhost:3000', // Alternative dev port
+      ]),
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
